596927661adbe2ca7c0ca340b932ca1b67bb163f
arenoit <davidrylacer@gmail.com>
2025-06-12 13:08:57 -0500

    Add API JWT

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        $user = \App\Models\User::where('email', $credentials['email'])->first();
+        if (!$user) {
+            return response()->json(['error' => 'Usuario no encontrado'], 404);
+        }
+
+        if (!\Hash::check($credentials['password'], $user->password)) {
+            return response()->json(['error' => 'Contraseña incorrecta'], 401);
+        }
+
+        if (!$token = JWTAuth::attempt($credentials)) {
+            return response()->json(['error' => 'Credenciales inválidas'], 401);
+        }
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 
 /*
@@ -17,6 +18,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    //Route::get('me', [AuthController::class, 'me']);
+    //Route::post('logout', [AuthController::class, 'logout']);
+    //Route::post('refresh', [AuthController::class, 'refresh']);
 });
 //Route::post('pos-provinces', 'ProvincesController@data_provinces');
 //Route::post('pos-registers', 'FormUserController@data_users');
